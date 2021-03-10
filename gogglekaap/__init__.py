9de64c14721dfa7335298622f4d5a2862f8ae46c
hidekuma <d.hidekuma@gmail.com>
2021-03-10 13:18:40 +0900

    플라스크 기본 라우터

--- gogglekaap/__init__.py
+++ gogglekaap/__init__.py
@@ -11,4 +11,32 @@ def create_app():
     def index():
         return 'Hello World'
 
+    ''' === Routing Practice === '''
+    from flask import jsonify, redirect, url_for
+    from markupsafe import escape
+
+    @app.route('/test/name/<name>')
+    def name(name):
+        return f'Name is {name}, {escape(type(name))}'
+
+    @app.route('/test/id/<int:id>')
+    def id(id):
+        return 'Id: %d' % id
+
+    @app.route('/test/path/<path:subpath>')
+    def path(subpath):
+        return subpath
+
+    @app.route('/test/json')
+    def json():
+        return jsonify({'hello': 'world'})
+
+    @app.route('/test/redirect/<path:subpath>')
+    def redirect_url(subpath):
+        return redirect(subpath)
+
+    @app.route('/test/urlfor/<path:subpath>')
+    def urlfor(subpath):
+        return redirect(url_for('path', subpath=subpath))
+
     return app
