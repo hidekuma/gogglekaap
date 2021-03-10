74f26d5a96f20f934842661f96a1eb3e11e013ad
hidekuma <d.hidekuma@gmail.com>
2021-03-10 21:58:43 +0900

    메모리로 로그인 재현

--- gogglekaap/routes/auth_route.py
+++ gogglekaap/routes/auth_route.py
@@ -3,12 +3,35 @@ from flask import (
     render_template,
     url_for,
     redirect,
+    request,
+    session,
     flash
 )
 from gogglekaap.forms.auth_form import LoginForm, RegisterForm
 
 NAME = 'auth'
 bp = Blueprint(NAME, __name__, url_prefix='/auth')
+
+
+''' === only for testing === '''
+from dataclasses import dataclass
+USERS = []
+@dataclass
+class User:
+    """
+    class User:
+        def __init__(self, user_id, user_name, password):
+            self.user_id = user_id
+            self.user_name = user_name
+            self.password = password
+    """
+    user_id: str
+    user_name: str
+    password: str
+
+USERS.append(User('hidekuma', 'hidekuma', '1234'))
+USERS.append(User('tester', 'tester', '1234'))
+USERS.append(User('admin', 'admin', '1234'))
 
 @bp.route('/')
 def index():
@@ -25,15 +48,29 @@ def login():
         # 3) 로그인 유지(세션)
         user_id = form.data.get('user_id')
         password = form.data.get('password')
-        return f'{user_id}, {password}'
+        user = [user for user in USERS if user.user_id == user_id]
+        if user:
+            user = user[0]
+            if user.password != password:
+                flash('Password is not valid.')
+            else:
+                session['user_id'] = user_id
+                return redirect(url_for('base.index'))
+        else:
+            flash('User ID is not exists.')
+        return redirect(request.path)
     else:
         flash_form_errors(form)
+
+    if session.get("user_id"):
+        return redirect(url_for('base.index'))
     return render_template(f'{NAME}/login.html', form=form)
 
 @bp.route('/logout')
 def logout():
     # TODO: 로그아웃 제거(세션)
-    return 'logout'
+    session.pop('user_id', None)
+    return redirect(url_for(f'{NAME}.login'))
 
 @bp.route('/register', methods=['GET', 'POST'])
 def register():
@@ -48,7 +85,19 @@ def register():
         password = form.data.get('password')
         repassword = form.data.get('repassword')
         user_name = form.data.get('user_name')
-        return f'{user_id}, {password}, {repassword}, {user_name}'
+
+        user = [user for user in USERS if user.user_id == user_id]
+        if user:
+            flash('User ID is already exsits.')
+            return redirect(request.path)
+        else:
+            USERS.append(User(
+                user_id = user_id,
+                user_name = user_name,
+                password = password
+            ))
+            session['user_id'] = user_id
+        return redirect(url_for('base.index'))
     else:
         flash_form_errors(form)
     return render_template(f'{NAME}/register.html', form=form)
