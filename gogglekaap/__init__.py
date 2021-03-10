ce55df3f80f651171c0bdaab96be27f0fadc44b1
hidekuma <d.hidekuma@gmail.com>
2021-03-10 15:52:23 +0900

    정적파일 추가 및 디버그 환경 캐시제거

--- gogglekaap/__init__.py
+++ gogglekaap/__init__.py
@@ -7,6 +7,10 @@ def create_app():
     print('run: create_app()')
     app = Flask(__name__)
 
+    if app.config['DEBUG'] == True:
+        app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 1
+        app.config['TEMPLATES_AUTO_RELOAD'] = True
+
     @app.route('/')
     def index():
         # app.logger.info('Run HelloWorld!')
