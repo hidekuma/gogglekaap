452fbe452a472ee88209b83c8ccde78bdec37dff
hidekuma <d.hidekuma@gmail.com>
2021-03-10 17:17:01 +0900

    Flask WTF으로 로그인/회원가입 폼 레이아웃 작성

--- gogglekaap/__init__.py
+++ gogglekaap/__init__.py
@@ -24,18 +24,45 @@ def create_app():
         return render_template('index.html')
 
     from gogglekaap.forms.auth_form import LoginForm, RegisterForm
-    @app.route('/auth/login')
+    @app.route('/auth/login', methods=['GET', 'POST'])
     def login():
         form = LoginForm()
+        if form.validate_on_submit():
+            # TODO
+            # 1) 유저조회
+            # 2) 존재하는 유저 인지 체크
+            # 3) 패스워드 정합확인
+            # 3) 로그인 유지(세션)
+            user_id = form.data.get('user_id')
+            password = form.data.get('password')
+            return f'{user_id}, {password}'
+        else:
+            # TODO: 에러컨트롤
+            pass
         return render_template('login.html', form=form)
 
     @app.route('/auth/logout')
     def logout():
+        # TODO: 로그아웃 제거(세션)
         return 'logout'
 
-    @app.route('/auth/register')
+    @app.route('/auth/register', methods=['GET', 'POST'])
     def register():
         form = RegisterForm()
+        if form.validate_on_submit():
+            # TODO
+            # 1) 유저조회
+            # 2) 유저 이미 존재하는지 체크
+            # 3) 없으면 유저 생성
+            # 4) 로그인 유지(세션)
+            user_id = form.data.get('user_id')
+            password = form.data.get('password')
+            repassword = form.data.get('repassword')
+            user_name = form.data.get('user_name')
+            return f'{user_id}, {password}, {repassword}, {user_name}'
+        else:
+            # TODO: 에러컨트롤
+            pass
         return render_template('register.html', form=form)
 
     @app.errorhandler(404)
