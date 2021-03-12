9d7752de34f1c09010bd29afc5d321994fa32ff4
hidekuma <d.hidekuma@gmail.com>
2021-03-12 15:15:34 +0900

    메모 추가 API 작성

--- gogglekaap/apis/memo.py
+++ gogglekaap/apis/memo.py
@@ -1,6 +1,6 @@
 from gogglekaap.models.memo import Memo as MemoModel
 from gogglekaap.models.user import User as UserModel
-from flask_restx import Namespace, Resource, fields
+from flask_restx import Namespace, Resource, fields, reqparse
 from flask import g
 
 ns = Namespace(
@@ -18,6 +18,10 @@ memo = ns.model('Memo', {
 })
 
 
+parser = reqparse.RequestParser()
+parser.add_argument('title', required=True, help='메모 제목')
+parser.add_argument('content', required=True, help='메모 내용')
+
 @ns.route("")
 class MemoList(Resource):
     @ns.marshal_list_with(memo, skip_none=True)
@@ -32,6 +36,21 @@ class MemoList(Resource):
             MemoModel.created_at.desc()
         ).limit(10).all()
         return data
+
+    @ns.marshal_list_with(memo, skip_none=True)
+    @ns.expect(parser)
+    def post(self):
+        '''메모 생성'''
+        args = parser.parse_args()
+        memo = MemoModel(
+            title=args['title'],
+            content=args['content'],
+            user_id=g.user.id
+        )
+        g.db.add(memo)
+        g.db.commit()
+        return memo, 201
+
 
 @ns.param("id", '메모 고유 번호')
 @ns.route("/<int:id>")
