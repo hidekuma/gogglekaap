cded9b9243f2dfdce673b4c1682f4e1fe0aecf36
hidekuma <d.hidekuma@gmail.com>
2021-03-12 14:54:51 +0900

    메모 복수 조회 API 작성

--- gogglekaap/apis/memo.py
+++ gogglekaap/apis/memo.py
@@ -1,7 +1,34 @@
 from gogglekaap.models.memo import Memo as MemoModel
-from flask_restx import Namespace
+from gogglekaap.models.user import User as UserModel
+from flask_restx import Namespace, Resource, fields
+from flask import g
 
 ns = Namespace(
     'memos',
     description='메모 관련 API'
 )
+
+memo = ns.model('Memo', {
+    'id': fields.Integer(required=True, description='메모 고유 아이디'),
+    'user_id': fields.Integer(required=True, description='메모 작성자 유저 고유 번호'),
+    'title': fields.String(required=True, description='메모 제목'),
+    'content': fields.String(required=True, description='메모 내용'),
+    'created_at': fields.DateTime(description='작성일'),
+    'updated_at': fields.DateTime(description='변경일')
+})
+
+
+@ns.route("")
+class MemoList(Resource):
+    @ns.marshal_list_with(memo, skip_none=True)
+    def get(self):
+        '''메모 복수 조회'''
+        data = MemoModel.query.join(
+            UserModel,
+            UserModel.id == MemoModel.user_id
+        ).filter(
+            UserModel.id == g.user.id,
+        ).order_by(
+            MemoModel.created_at.desc()
+        ).limit(10).all()
+        return data
