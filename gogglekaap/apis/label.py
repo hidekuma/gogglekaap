30bf8a32f6c7259e266a146a5d02742bc7a14f3d
hidekuma <d.hidekuma@gmail.com>
2021-03-14 17:53:41 +0900

    라벨 복수 조회 API 작성

--- gogglekaap/apis/label.py
+++ gogglekaap/apis/label.py
@@ -22,6 +22,17 @@ parser.add_argument('content', required=True, type=str, help="라벨 내용")
 @ns.route('')
 class LabelList(Resource):
     @ns.marshal_list_with(label, skip_none=True)
+    def get(self):
+        '''라벨 복수 조회'''
+        pages = LabelModel.query.join(
+            UserModel,
+            UserModel.id == LabelModel.user_id
+        ).filter(
+            UserModel.id == g.user.id,
+        )
+        return pages.all()
+
+    @ns.marshal_list_with(label, skip_none=True)
     @ns.expect(parser)
     def post(self):
         '''라벨 생성'''
