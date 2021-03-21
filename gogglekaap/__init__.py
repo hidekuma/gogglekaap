108402805e3338ca65c2d2a2b791ebbae501f1d8
hidekuma <d.hidekuma@gmail.com>
2021-03-21 19:16:22 +0900

    --불필요한 테스트 코드 주석처리

--- gogglekaap/__init__.py
+++ gogglekaap/__init__.py
@@ -1,88 +1,11 @@
 from flask import Flask
 
-db = 'database'
-
 def create_app():
     print('run: create_app()')
     app = Flask(__name__)
 
     @app.route('/')
     def index():
-        app.logger.info('RUN HELLOWORLD!')
         return "Hello World and Python!"
-
-
-    ''' === Routing Practices === '''
-    from markupsafe import escape
-    from flask import redirect, jsonify, url_for
-
-    @app.route('/test/name/<string:name>')
-    def name(name):
-        return f'My name is {name}, {escape(type(name))}'
-
-    @app.route('/test/id/<int:id>')
-    def id(id):
-        return 'ID is %d' % id
-
-    @app.route('/test/path/<path:subpath>')
-    def path(subpath):
-        return subpath
-
-    @app.route('/test/json')
-    def json():
-        return jsonify([{'Hello': 'world'}])
-
-    @app.route('/test/redirect/<path:subpath>')
-    def redirect_url(subpath):
-        return redirect(subpath)
-
-    @app.route('/test/urlfor')
-    def urlfor():
-        return redirect(url_for('name', name='hidekuma'))
-
-    ''' === Request hook, Context controll === '''
-    from flask import g, current_app
-
-    @app.before_first_request
-    def before_first_request():
-        app.logger.info('BEFORE_FIRST_REQUEST')
-
-    @app.before_request
-    def before_request():
-        g.test = True
-        app.logger.info("BEFORE_REQUEST")
-
-    @app.after_request
-    def after_request(response):
-        print(g.test)
-        print('mode:', current_app.config['DEBUG'])
-        print(app.config['DEBUG'])
-        g.test = False
-        app.logger.info('AFTER_REQUEST')
-        return response
-
-    @app.teardown_request
-    def teardown_request(exception):
-        print(g.test)
-        app.logger.info('TEARDOWN_REQUEST')
-
-    @app.teardown_appcontext
-    def teardown_appcontext(exception):
-        app.logger.info('TEARDOWN_APPCONTEXT')
-
-    ''' === Method & Request context Practice === '''
-    from flask import request
-    @app.route('/test/method', methods=['GET', 'POST'])
-    def method_test():
-        return jsonify({
-            'request.host': request.host,
-            'request.path': request.path,
-            'request.url': request.url,
-            'request.args': request.args,
-            'request.form': request.form,
-            'request.form.test': request.form.get('test'),
-            'request.json': request.json,
-            'requset.method': request.method
-        })
 
     return app
--- gogglekaap/test.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-sys.path.append('.')
-
-from gogglekaap import db
-
-print('gogglekaap db imported', db)
