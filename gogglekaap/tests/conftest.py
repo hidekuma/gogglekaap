f6ca2f720c7b3ee98b5c4daf0533df08518785b5
hidekuma <d.hidekuma@gmail.com>
2021-03-24 17:05:37 +0900

    ~B.2. 세션 기반 인증 흐름 구현: 테스트 케이스 세션적용과 스콥정의 및 ini 설정

--- gogglekaap/tests/conftest.py
+++ gogglekaap/tests/conftest.py
@@ -2,11 +2,12 @@ import sys
 sys.path.append('.')
 
 import pytest
+import os
 from gogglekaap.configs import TestingConfig
 from gogglekaap import create_app, db
 from gogglekaap.models.user import User as UserModel
 
-@pytest.fixture
+@pytest.fixture(scope='session')
 def user_data():
     yield dict(
         user_id='tester',
@@ -14,7 +15,7 @@ def user_data():
         password='tester',
     )
 
-@pytest.fixture
+@pytest.fixture(scope='session')
 def app(user_data):
     app = create_app(TestingConfig())
     with app.app_context():
@@ -22,11 +23,20 @@ def app(user_data):
         db.create_all()
         db.session.add(UserModel(**user_data))
         db.session.commit()
-    yield app
+        yield app
 
+        db.drop_all()
+        db_path = app.config['SQLALCHEMY_DATABASE_URI'].replace(
+            'sqlite:///',
+            ''
+        )
+        if os.path.isfile(db_path):
+            os.remove(db_path)
 
-@pytest.fixture
-def client(app):
+@pytest.fixture(scope='session')
+def client(app, user_data):
     with app.test_client() as client:
+        with client.session_transaction() as sess:
+            sess['user_id'] = user_data['user_id']
         yield client
 
