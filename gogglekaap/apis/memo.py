f87d93284c32f6aebe7489327153b96d88365e06
hidekuma <d.hidekuma@gmail.com>
2021-03-28 23:39:37 +0900

    ~F.2. 메모 기능 업그레이드: 메모 이미지 추가 API 작성 및 컬럼 추가, DB 마이그레이션

--- gogglekaap/apis/memo.py
+++ gogglekaap/apis/memo.py
@@ -1,7 +1,11 @@
 from gogglekaap.models.memo import Memo as MemoModel
 from gogglekaap.models.user import User as UserModel
 from flask_restx import Namespace, Resource, fields, reqparse
-from flask import g
+from flask import g, current_app
+from werkzeug.utils import secure_filename
+from werkzeug.datastructures import FileStorage
+import os
+
 
 ns = Namespace(
     'memos',
@@ -13,6 +17,7 @@ memo = ns.model('memo', {
     'user_id': fields.Integer(required=True, description='메모 작성자 유저 고유 번호'),
     'title': fields.String(required=True, description='메모 제목'),
     'content': fields.String(required=True, description='메모 내용'),
+    'linked_image': fields.String(required=False, description='메모 이미지'),
     'created_at': fields.DateTime(description='생성일자'),
     'updated_at': fields.DateTime(description='갱신일자')
 })
@@ -20,6 +25,7 @@ memo = ns.model('memo', {
 parser = reqparse.RequestParser()
 parser.add_argument('title', required=True, help='메모 제목')
 parser.add_argument('content', required=True, help='메모 내용')
+parser.add_argument('linked_image', location='files', type=FileStorage, required=False, help='메모 이미지')
 
 put_parser = parser.copy()
 put_parser.replace_argument('title', required=False, help='메모 제목')
@@ -28,6 +34,43 @@ put_parser.replace_argument('content', required=False, help='메모 내용')
 get_parser = reqparse.RequestParser()
 get_parser.add_argument('page', required=False, type=int, help='메모 페이지 번호')
 get_parser.add_argument('needle', required=False, help='메모 검색어')
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in {'jpg', 'jpeg', 'png', 'gif'}
+
+def randomword(length):
+    import random, string
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(length))
+
+def save_file(file):
+    if file.filename == '':
+        ns.abort(400)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        relative_path = os.path.join(
+            current_app.static_url_path[1:],
+            current_app.config['USER_STATIC_BASE_DIR'],
+            g.user.user_id,
+            'memos',
+            randomword(10),
+            filename
+        )
+        upload_path = os.path.join(
+            current_app.root_path,
+            relative_path
+        )
+        os.makedirs(
+            os.path.dirname(upload_path),
+            exist_ok=True
+        )
+        file.save(upload_path)
+        return relative_path, upload_path
+    else:
+        ns.abort(400)
+
+
 
 @ns.route('')
 class MemoList(Resource):
@@ -68,9 +111,14 @@ class MemoList(Resource):
         """메모 생성"""
         args = parser.parse_args()
         memo = MemoModel()
+        file = args['linked_image']
         memo.title = args['title']
         memo.content = args['content']
         memo.user_id = g.user.id
+
+        if file:
+            relative_path, _ = save_file(file)
+            memo.linked_image = relative_path
         g.db.add(memo)
         g.db.commit()
         return memo, 201
--- gogglekaap/models/memo.py
+++ gogglekaap/models/memo.py
@@ -5,6 +5,7 @@ class Memo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     content = db.Column(db.Text, nullable=False)
+    linked_image = db.Column(db.String(200), nullable=True)
     created_at = db.Column(db.DateTime(), default=func.now())
     updated_at = db.Column(
         db.DateTime(),
