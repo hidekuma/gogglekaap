8d69109b41d54624192d0ef41d7101c550159e4c
hidekuma <d.hidekuma@gmail.com>
2021-03-30 14:16:27 +0900

    ~H.2. 라벨 기본 기능 구현: 라벨 기본 테스트 케이스 작성

---
+++ gogglekaap/tests/test_api_label.py
@@ -0,0 +1,30 @@
+def test_get_labels(client):
+    r = client.get(
+        '/api/labels',
+        follow_redirects=True
+    )
+    assert r.status_code == 200
+    assert r.json == []
+
+def test_post_label(client):
+    data = {
+        'content': 'label'
+    }
+    r = client.post(
+        '/api/labels',
+        data=data
+    )
+    assert r.status_code == 201
+    assert r.json.get('content') == data['content']
+
+def test_delete_label(client):
+    r = client.delete(
+        '/api/labels/1'
+    )
+    assert r.status_code == 204
+    r = client.get(
+        '/api/labels',
+        follow_redirects=True
+    )
+    assert r.status_code == 200
+    assert r.json == []
