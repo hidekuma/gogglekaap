eeefc0e0212c5456765fa98a93ef3a94eaaf991d
hidekuma <d.hidekuma@gmail.com>
2021-03-30 14:16:12 +0900

    ~H.1. 라벨 기본 기능 구현: 라벨 모델생성과 API 연결, DB 마이그레이션

--- gogglekaap/apis/__init__.py
+++ gogglekaap/apis/__init__.py
@@ -2,6 +2,7 @@ from flask import Blueprint, g, abort
 from flask_restx import Api
 from .user import ns as UserNamespace
 from .memo import ns as MemoNamespace
+from .label import ns as LabelNamespace
 from functools import wraps
 
 blueprint = Blueprint('api', __name__, url_prefix='/api')
@@ -26,3 +27,4 @@ api = Api(
 
 api.add_namespace(UserNamespace)
 api.add_namespace(MemoNamespace)
+api.add_namespace(LabelNamespace)
---
+++ gogglekaap/apis/label.py
@@ -0,0 +1,11 @@
+from flask_restx import Namespace, fields
+from gogglekaap.models.label import Label as LabelModel
+
+ns = Namespace('labels', description='라벨 관련 API')
+
+label = ns.model('label', {
+    'id': fields.Integer(requird=True, description='라벨 고유 아이디'),
+    'user_id': fields.Integer(requird=True, description='라벨 작성자 유저 고유 아이디'),
+    'content': fields.String(required=True, description='라벨 내용'),
+    'created_at': fields.DateTime(description='라벨 생성일자')
+})
