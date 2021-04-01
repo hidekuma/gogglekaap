c75552db89e08dedb23a0433293bc79ffa0cc991
hidekuma <d.hidekuma@gmail.com>
2021-04-01 12:39:23 +0900

    ~K.4. 도커라이징: 도커 컴포즈에 nginx 추가하기

--- etc/docker-entrypoint.sh
+++ etc/docker-entrypoint.sh
@@ -3,4 +3,5 @@ set -e
 
 flask db upgrade
 
-gunicorn --bind :8080 --workers 2 --threads 8 --access-logfile - 'gogglekaap:create_app()'
+#gunicorn --bind :8080 --workers 2 --threads 8 --access-logfile - 'gogglekaap:create_app()'
+gunicorn --bind unix:/tmp/gunicorn.sock --workers 2 --threads 8 --access-logfile - 'gogglekaap:create_app()'
