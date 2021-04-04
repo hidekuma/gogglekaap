456eea37978d0b2ef80186a40eabc2fe0fa766e3
hidekuma <d.hidekuma@gmail.com>
2021-04-04 12:06:11 +0900

    1.6.0

--- gogglekaap/__init__.py
+++ gogglekaap/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask
-
-db = 'database'
+from flask import render_template
 
 def create_app():
     print('run: create_app()')
@@ -8,75 +7,10 @@ def create_app():
 
     @app.route('/')
     def index():
-        app.logger.info('RUN HELLOWORLD')
-        return 'hello world'
+        return render_template('index.html')
 
-    ''' Routing Practice '''
-    from flask import jsonify, redirect, url_for
-    from markupsafe import escape
-
-    @app.route('/test/name/<name>')
-    def name(name):
-        return f'Name is {name}, {escape(type(name))}'
-
-    @app.route('/test/id/<int:id>')
-    def id(id):
-        return 'Id: %d' % id
-
-    @app.route('/test/path/<path:subpath>')
-    def path(subpath):
-        return subpath
-
-    @app.route('/test/json')
-    def json():
-        return jsonify({'hello': 'world'})
-
-    @app.route('/test/redirect/<path:subpath>')
-    def redirect_url(subpath):
-        return redirect(subpath)
-
-    @app.route('/test/urlfor/<path:subpath>')
-    def urlfor(subpath):
-        return redirect(url_for('path', subpath=subpath))
-
-    ''' Request Hook '''
-    from flask import g, current_app
-    @app.before_first_request
-    def before_first_request():
-        app.logger.info('BEFORE_FIRST_REQUEST')
-
-    @app.before_request
-    def before_request():
-        g.test=True
-        app.logger.info('BEFORE_REQUEST')
-
-    @app.after_request
-    def after_request(response):
-        app.logger.info(f'g.test:{g.test}')
-        # app.logger.info(f'current_app.config:{current_app.config}')
-        app.logger.info('AFTER_REQUEST')
-        return response
-
-    @app.teardown_request
-    def teardown_request(exception):
-        app.logger.info('TEARDOWN_REQUEST')
-
-    @app.teardown_appcontext
-    def teardown_appcontext(exception):
-        app.logger.info('TEARDOWN_CONTEXT')
-
-
-    '''Method'''
-    from flask import request
-
-    @app.route('/test/method/<id>', methods=['GET', 'POST', 'DELETE', 'PUT'])
-    def method_test(id):
-        return jsonify({
-            'request.method': request.method,
-            'request.args': request.args,
-            'request.form': request.form,
-            'request.json': request.json,
-        })
-
+    @app.errorhandler(404)
+    def page_404(error):
+        return render_template('/404.html'), 404
 
     return app
