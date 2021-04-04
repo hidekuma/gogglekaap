ef805fd49c03c9b36179a9ed7ccd1b8d38b9670e
hidekuma <d.hidekuma@gmail.com>
2021-04-04 12:31:27 +0900

    1.7.0

--- gogglekaap/__init__.py
+++ gogglekaap/__init__.py
@@ -1,13 +1,39 @@
 from flask import Flask
 from flask import render_template
+from flask_wtf.csrf import CSRFProtect
+
+csrf = CSRFProtect()
 
 def create_app():
     print('run: create_app()')
     app = Flask(__name__)
 
+    app.config['SECRET_KEY'] = 'secretkey'
+
+    if app.config['DEBUG']:
+        app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 1
+
+    '''CSRF INIT'''
+    csrf.init_app(app)
+
     @app.route('/')
     def index():
         return render_template('index.html')
+
+    from gogglekaap.forms.auth_form import LoginForm, RegisterForm
+    @app.route('/auth/login')
+    def login():
+        form = LoginForm()
+        return render_template('login.html', form=form)
+
+    @app.route('/auth/register')
+    def register():
+        form = RegisterForm()
+        return render_template('register.html', form=form)
+
+    @app.route('/auth/logout')
+    def logout():
+        return 'logout'
 
     @app.errorhandler(404)
     def page_404(error):
---
+++ gogglekaap/forms/auth_form.py
@@ -0,0 +1,7 @@
+from flask_wtf import FlaskForm
+
+class LoginForm(FlaskForm):
+    pass
+
+class RegisterForm(FlaskForm):
+    pass
