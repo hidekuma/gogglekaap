f3d16b57239fd2da9e5f5fb400555548e8abef49
hidekuma <d.hidekuma@gmail.com>
2021-04-11 15:23:50 +0900

    1.13.0

--- gogglekaap/__init__.py
+++ gogglekaap/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from flask import render_template
 from flask_wtf.csrf import CSRFProtect
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 csrf = CSRFProtect()
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():
     print('run: create_app()')
@@ -10,9 +14,18 @@ def create_app():
 
     app.config['SECRET_KEY'] = 'secretkey'
     app.config['SESSION_COOKIE_NAME'] = 'gogglekaap'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@localhost/gogglekaap?charset=utf8'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     if app.config['DEBUG']:
         app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 1
+
+    '''DB INIT'''
+    db.init_app(app)
+    if app.config['SQLALCHEMY_DATABASE_URI'].startswith('sqlite'):
+        migrate.init_app(app, db, render_as_batch=True)
+    else:
+        migrate.init_app(app, db)
 
     '''Routes INIT'''
     from gogglekaap.routes import base_route, auth_route
