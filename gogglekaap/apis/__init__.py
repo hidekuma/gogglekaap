490c350557cbef6fb4320699670d5c9a05e49f11
hidekuma <d.hidekuma@gmail.com>
2021-04-23 13:21:15 +0900

    2.3.0

--- gogglekaap/apis/__init__.py
+++ gogglekaap/apis/__init__.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, g, abort
 from flask_restx import Api
 from .user import ns as UserNamespace
+from .memo import ns as MemoNamespace
 from functools import wraps
 
 def check_session(func):
@@ -27,3 +28,4 @@ api = Api(
 )
 
 api.add_namespace(UserNamespace)
+api.add_namespace(MemoNamespace)
