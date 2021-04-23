8b7e944bfed7a38642a54e8569eb239be0764af5
hidekuma <d.hidekuma@gmail.com>
2021-04-23 12:11:13 +0900

    1.23.0

--- gogglekaap/tests/test_route.py
+++ gogglekaap/tests/test_route.py
@@ -1,17 +1,3 @@
-import sys
-sys.path.append('.')
-
-from gogglekaap.configs import TestingConfig
-from gogglekaap import create_app
-import pytest
-
-@pytest.fixture
-def client():
-    app = create_app(TestingConfig())
-
-    with app.test_client() as client:
-        yield client
-
 def test_auth(client):
     r = client.get(
         '/auth/register',
