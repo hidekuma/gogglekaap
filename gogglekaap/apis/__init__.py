60e95de5aa1908e6aa26bff0769853b2ce104370
hidekuma <d.hidekuma@gmail.com>
2021-04-23 17:58:06 +0900

    2.23.0

--- gogglekaap/apis/__init__.py
+++ gogglekaap/apis/__init__.py
@@ -2,6 +2,7 @@ from flask import Blueprint, g, abort
 from flask_restx import Api
 from .user import ns as UserNamespace
 from .memo import ns as MemoNamespace
+from .label import ns as LabelNamespace 
 from functools import wraps
 
 def check_session(func):
@@ -29,3 +30,4 @@ api = Api(
 
 api.add_namespace(UserNamespace)
 api.add_namespace(MemoNamespace)
+api.add_namespace(LabelNamespace)
---
+++ gogglekaap/apis/label.py
@@ -0,0 +1,14 @@
+from flask_restx import Namespace, fields
+from gogglekaap.models.label import Label as LabelModel
+
+ns = Namespace(
+    'labels',
+    description='라벨 관련 API'
+)
+
+label = ns.model('Label',{
+    'id': fields.Integer(required=True, description="라벨 고유 아이디"),
+    'user_id': fields.Integer(required=True, description="라벨 작성자 유저 고유 아이디"),
+    'content': fields.String(required=True, description="라벨 내용"),
+    'created_at': fields.DateTime(description="라벨 생성 일자")
+})
