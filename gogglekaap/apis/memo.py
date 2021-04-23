0b82e707aa08cc551c887f59b5a20964385c4a12
hidekuma <d.hidekuma@gmail.com>
2021-04-23 14:00:38 +0900

    2.7.0

--- gogglekaap/apis/memo.py
+++ gogglekaap/apis/memo.py
@@ -1,6 +1,6 @@
 from gogglekaap.models.memo import Memo as MemoModel
 from gogglekaap.models.user import User as UserModel 
-from flask_restx import Namespace, fields, Resource
+from flask_restx import Namespace, fields, Resource, reqparse
 from flask import g
 
 ns = Namespace(
@@ -17,6 +17,10 @@ memo = ns.model('Memo', {
     'updated_at': fields.DateTime(description='메모 변경일'),
 })
 
+parser = reqparse.RequestParser()
+parser.add_argument('title', required=True, help='메모 제목')
+parser.add_argument('content', required=True, help='메모 내용')
+
 @ns.route('')
 class MemoList(Resource):
 
@@ -32,6 +36,20 @@ class MemoList(Resource):
             MemoModel.created_at.desc()
         ).limit(10).all()
         return data
+
+    @ns.marshal_list_with(memo, skip_none=True)
+    @ns.expect(parser)
+    def post(self):
+        '''메모 생성'''
+        args = parser.parse_args()
+        memo = MemoModel(
+            title=args['title'],
+            content=args['content'],
+            user_id=g.user.id
+        )
+        g.db.add(memo)
+        g.db.commit()
+        return memo, 201
 
 @ns.param('id', '메모 고유 아이디')
 @ns.route('/<int:id>')
