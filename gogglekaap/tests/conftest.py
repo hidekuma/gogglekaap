acaa2d1d13e6823d4e6142e5314eafc1f00d6764
hidekuma <d.hidekuma@gmail.com>
2021-04-23 13:08:52 +0900

    2.2.0

--- gogglekaap/tests/conftest.py
+++ gogglekaap/tests/conftest.py
@@ -5,8 +5,9 @@ from gogglekaap.configs import TestingConfig
 from gogglekaap import create_app, db
 from gogglekaap.models.user import User as UserModel
 import pytest
+import os
 
-@pytest.fixture
+@pytest.fixture(scope='session')
 def user_data():
     yield dict(
         user_id='tester',
@@ -14,7 +15,7 @@ def user_data():
         password='tester'
     )
 
-@pytest.fixture
+@pytest.fixture(scope='session')
 def app(user_data):
     app = create_app(TestingConfig())
     with app.app_context():
@@ -22,10 +23,20 @@ def app(user_data):
         db.create_all()
         db.session.add(UserModel(**user_data))
         db.session.commit()
-    yield app
+        yield app
+        # 불필요 디비 정리
+        db.drop_all()
+        db_path = app.config['SQLALCHEMY_DATABASE_URI'].replace(
+            'sqlite:///',
+            ''
+        )
+        if os.path.isfile(db_path):
+            os.remove(db_path)
 
-
-@pytest.fixture
-def client(app):
+@pytest.fixture(scope='session')
+def client(app, user_data):
     with app.test_client() as client:
+        # NOTE: 세션 입혀주기
+        with client.session_transaction() as session:
+            session['user_id'] = user_data.get('user_id')
         yield client
