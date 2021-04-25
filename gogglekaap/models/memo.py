9194f6aaeae036c31d21f44c465d859d1352b2bc
hidekuma <d.hidekuma@gmail.com>
2021-04-25 14:04:02 +0900

    2.29.0

--- gogglekaap/models/memo.py
+++ gogglekaap/models/memo.py
@@ -1,6 +1,13 @@
 from gogglekaap import db
 from sqlalchemy import func
 
+memos_labels = db.Table(
+    'memos_labels',
+    db.Column('memo_id', db.Integer, db.ForeignKey('memo.id'), primary_key=True),
+    db.Column('label_id', db.Integer, db.ForeignKey('label.id'), primary_key=True)
+)
+
+
 class Memo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
@@ -16,4 +23,9 @@ class Memo(db.Model):
             ondelete='CASCADE'
         ),
         nullable=False
+    )
+    labels = db.relationship(
+        'Label', # 라벨과의 연관성을 가져라
+        secondary=memos_labels, # 다대다 관계 테이블을 참조하라
+        backref=db.backref('memos') # Label객체에서도 memos객체 접근하도록 처리하라
     )
